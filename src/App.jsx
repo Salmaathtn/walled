6f90c027c4f785b08bd39572a763b61a004bd960
Salmaathtn <salmaathoetani@gmail.com>
2024-12-09 12:08:56 +0700

    membuat navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import logo from "./assets/logo.png";
+import NavItems from "./component/navitems";
 
-import "./App.css";
 
-export default function App() {
+function App() {
+
+  const menu = [
+    {
+      title: "Dashboard",
+      link: "#"
+    },
+    {
+      title: "Transfer",
+      link: "#"
+    },
+    {
+      title: "Topup",
+      link: "#"
+    },
+    {
+      title: "Signout",
+      link: "#"
+    },
+  ]
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <nav className="fixed top-0 flex items-center w-screen justify-between bg-white py-4 px-8">
+        <img src={logo} />
+        <div className="flex gap-x-8">
+          <NavItems menu={menu} />
+        </div>
+      </nav>
+    </>
+  );
 }
+
+export default App;
